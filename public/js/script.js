ff38e7b7a3f5d10cc5d5e785b4e50410d576ce91
sriyavallepu <s224684661@deakin.edu.au>
2024-08-02 22:39:06 +1000

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+const clickMe = () => {
+    alert("Thanks for clicking me. Hope you have a nice day!")
+}
+$(document).ready(function () {
+    // $('.materialboxed').materialbox();
+    $('#clickMeButton').click(() => {
+        clickMe();
+        
+})
+});
+
